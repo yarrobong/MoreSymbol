a6cd5661c8275390bbe70fe1796525693ec8a455
yarrobong <oneboardsholi@gmail.com>
2023-10-26 13:22:17 +0500

    Добавьте файлы проекта.

---
+++ MoreSymbol/Program.cs
@@ -0,0 +1,14 @@
+﻿string str = Console.ReadLine();
+int uniqueCount = 0;
+string uniqueCharacters = "";
+
+foreach (char c in str)
+{
+    if (uniqueCharacters.IndexOf(c) == -1)
+        {
+            uniqueCharacters += c;
+            uniqueCount++;
+        }
+}
+
+Console.WriteLine(uniqueCount);
